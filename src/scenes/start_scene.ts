18a98970185a081163c47bb4bc3bcda58da9ace2
Slava Yefremov <habdenscrimen@gmail.com>
2022-04-28 21:12:52 +0300

    feat: if contact already saved, don't request it

--- src/scenes/start_scene.ts
+++ src/scenes/start_scene.ts
@@ -6,18 +6,32 @@ export const startScene = new Scenes.BaseScene<CustomContext>(scenes.START)
 
 // enter the scene
 startScene.enter(async (ctx) => {
-  console.debug('start scene: enter')
+  try {
+    console.debug('start scene: enter')
 
-  await ctx.reply(ctx.config.messages.scenes.start.enter, {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: 'Обрати стікери', callback_data: scenes.REQUEST_CONTACT },
-          { text: 'У мене є питання', callback_data: scenes.QUESTIONS },
+    // check if contact exists in database
+    const snapshot = await ctx.database.ref(`users/${ctx.from!.id}`).get()
+    const contactExists = snapshot.exists()
+
+    await ctx.reply(ctx.config.messages.scenes.start.enter, {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: 'Обрати стікери',
+              // if contact already saved, select stickers (skip contact request)
+              callback_data: contactExists
+                ? scenes.SELECT_STICKERS
+                : scenes.REQUEST_CONTACT,
+            },
+            { text: 'У мене є питання', callback_data: scenes.QUESTIONS },
+          ],
         ],
-      ],
-    },
-  })
+      },
+    })
+  } catch (error) {
+    console.error(`failed to enter start scene: ${error}`)
+  }
 })
 
 // leave the scene

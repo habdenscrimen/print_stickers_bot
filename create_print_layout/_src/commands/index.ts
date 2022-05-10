c9f45a67e47a79048bc99980a8e67cc94743fa0e
Slava Yefremov <habdenscrimen@gmail.com>
2022-05-10 19:32:24 +0300

    chore(process-images): remove unused files

--- create_print_layout/_src/commands/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './prepare_confirmed_orders_images'
-export * from './create_print_layouts'
-export * from './count_print_ready_images'
--- create_print_layout/_src/layout/index.ts
+++
@@ -1,2 +0,0 @@
-export { getSizingInPX } from './get_sizing_in_px'
-export { mergeSVGs } from './merge_svgs'
